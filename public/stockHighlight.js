802bfbcc6ddb90142c07495a201b72d39af40706
arnaviny <109102974+arnaviny@users.noreply.github.com>
2023-08-19 20:05:30 +0300

    Your commit message

--- public/stockHighlight.js
+++ public/stockHighlight.js
@@ -3,12 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function highlightStockStatus(book, bookDiv) {
-    if (book.in_stock === 't') {
+    if (book.in_stock === true || book.in_stock === 'true') {
         bookDiv.style.position = "relative";
-        bookDiv.style.boxShadow = "0 0 15px 5px rgba(255, 0, 0, 0.4)"; // Green shadow
+        bookDiv.style.boxShadow = "0 0 15px 5px rgba(0, 255, 0, 0.4)"; // Green shadow for in-stock books
     } else {
         bookDiv.style.position = "relative";
-        bookDiv.style.boxShadow = "0 0 15px 5px rgba(0, 255, 0, 0.4)"; // Red shadow
+        bookDiv.style.boxShadow = "0 0 15px 5px rgba(255, 0, 0, 0.4)"; // Red shadow for out-of-stock books
     }
+    
 }
+
 
